514c1664c0d321dd5e37c1e5a9c8faca8eb417ec
Erik Pohl <epohl@dmigs.com>
2024-12-06 00:31:37 -0500

    refactored code to reduce number of lines

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 
 
 if __name__ == '__main__':
-    frankenstein_gutenberg_text = requests.get('https://www.gutenberg.org/cache/epub/42324/pg42324.txt')
-    sentence_list = frankenstein_gutenberg_text.text.split('.')
+    sentence_list = requests.get(
+        'https://www.gutenberg.org/cache/epub/42324/pg42324.txt'
+    ).text.split('.')
     print(sentence_list[random.randint(0, len(sentence_list)-1)])
