09381b1385fbe4a24662681a727bd60403254b64
Erik Pohl <epohl@dmigs.com>
2024-12-06 14:52:36 -0500

    use a pickle file to cache results daily or read from api if needed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import requests
 import random
+import os
+import datetime
+import pickle
 
 
 def get_all_books():
@@ -16,9 +19,31 @@ def get_all_books():
 
 
 if __name__ == '__main__':
-    book_list = get_all_books()
-    book = book_list[random.randint(0, len(book_list)-1)]
-    sentence_list = requests.get(
-        book['formats']['text/plain; charset=us-ascii']
-    ).text.split('.')
-    print(book['title'], sentence_list[random.randint(0, len(sentence_list)-1)])
+    mtime_datetime = None
+    try:
+        mtime = os.path.getmtime('books.pkl')
+        mtime_datetime = datetime.datetime.fromtimestamp(mtime)
+    except OSError:
+        mtime = None
+
+    now = datetime.datetime.now()
+
+    if not mtime or (mtime_datetime - now).seconds > 24*60*60:
+        # print("pickle file does not exist or is too stale")
+        book_list = get_all_books()
+        with open("books.pkl", "wb") as fhandle:
+            pickle.dump(book_list, fhandle)
+    else:
+        # print("shortcut: reading from relevant pickle file")
+        with open('books.pkl', "rb") as fhandle:
+            book_list = pickle.load(fhandle)
+    if not book_list:
+        raise OSError
+    written = False
+    while not written:
+        book = book_list[random.randint(0, len(book_list)-1)]
+        sentence_list = requests.get(
+            book['formats']['text/plain; charset=us-ascii']
+        ).text.split('.')
+        print(book['title'], sentence_list[random.randint(0, len(sentence_list)-1)])
+        written = True
