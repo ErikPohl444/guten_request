d036cccffa308456a3715d812734efdaa8e2de8c
Erik Pohl <epohl@dmigs.com>
2024-12-05 23:56:06 -0500

    refactored code to reduce number of lines by utilizing split

--- main.py
+++ main.py
@@ -2,22 +2,7 @@ import requests
 import random
 
 
-def get_sentence(contents, marker):
-    new_sentence = ''
-    while contents[marker] != '.':
-        new_sentence += contents[marker]
-        marker += 1
-    new_sentence += contents[marker]
-    marker += 1
-    return new_sentence, marker
-
-
 if __name__ == '__main__':
     frankenstein_gutenberg_text = requests.get('https://www.gutenberg.org/cache/epub/42324/pg42324.txt')
-    index = 0
-    count = random.randint(0, frankenstein_gutenberg_text.text.count('.') - 1)
-    sentence = ''
-    while count >= 0:
-        sentence, index = get_sentence(frankenstein_gutenberg_text.text, index)
-        count -= 1
-    print(sentence)
+    sentence_list = frankenstein_gutenberg_text.text.split('.')
+    print(sentence_list[random.randint(0, len(sentence_list)-1)])
