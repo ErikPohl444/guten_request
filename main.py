4bca694d51b82105b5a93c582e98687af3d217f7
Erik Pohl <epohl@dmigs.com>
2024-12-06 15:32:03 -0500

    encapsulated stale cache check

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import pickle
 import json
 
 
-def get_all_books(start_url):
+def get_all_books(start_url, cache_fname):
     url = start_url
     current_list = requests.get(url)
     long_list = []
@@ -16,7 +16,19 @@ def get_all_books(start_url):
         url = current_list.json()["next"]
         current_list = requests.get(url)
         count -= 1
+    with open(cache_fname, "wb") as fhandle:
+        pickle.dump(book_list, fhandle)
     return long_list
+
+
+def is_stale_cache(cache_fname):
+    mtime_datetime = None
+    try:
+        mtime = os.path.getmtime(cache_fname)
+        mtime_datetime = datetime.datetime.fromtimestamp(mtime)
+    except OSError:
+        mtime = None
+    return not mtime or (mtime_datetime - datetime.datetime.now()).seconds > books_stale_after_secs
 
 
 if __name__ == '__main__':
@@ -27,20 +39,8 @@ if __name__ == '__main__':
         books_url = config["books_url"]
         books_stale_after_secs = int(config["books_stale_after_secs"])
 
-
-    mtime_datetime = None
-    try:
-        mtime = os.path.getmtime(cache_fname)
-        mtime_datetime = datetime.datetime.fromtimestamp(mtime)
-    except OSError:
-        mtime = None
-
-    now = datetime.datetime.now()
-
-    if not mtime or (mtime_datetime - now).seconds > books_stale_after_secs :
-        book_list = get_all_books(books_url)
-        with open(cache_fname, "wb") as fhandle:
-            pickle.dump(book_list, fhandle)
+    if is_stale_cache(cache_fname):
+        book_list = get_all_books(books_url, cache_fname)
     else:
         with open(cache_fname, "rb") as fhandle:
             book_list = pickle.load(fhandle)
